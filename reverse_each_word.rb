1a9d2efcc1e6250d962fc43ce19b514922465b09
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-11 20:19:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word (str)
 
   new_string = []
    
-str.split.each { |word| new_string << word.reverse.join("")}
+str.split.each { |word| new_string << word.reverse}
   #binding.pry  
 #puts "hello"
 new_string
